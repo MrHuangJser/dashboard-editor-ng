fceca1e055c7bf5456f43a75324924b5e8b929fa
MrHuangJser <565628147@qq.com>
2019-07-24 10:41:41 +0800

    MrHuangJser - add item panel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ItemPanelComponent } from './layout/item-panel/item-panel.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+
+const COMPONENTS = [ItemPanelComponent];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ...COMPONENTS],
+  imports: [BrowserModule, NgZorroAntdModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/layout/item-panel/item-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'item-panel',
+  templateUrl: './item-panel.component.html',
+  styleUrls: ['./item-panel.component.less']
+})
+export class ItemPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
